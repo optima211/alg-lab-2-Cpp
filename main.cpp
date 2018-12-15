277bc83fd9c0e764098b7ad09e1e66050c06731d
optima211 <nedmk@mail.ru>
2018-12-15 17:26:50 +0700

    first work stack

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+#include <stack>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cout << "insert number!" << endl;
+    cin >> n;
+    stack<int> stk;
+    //возрастающее заполнение
+    for (int i=0; i < n; i++){
+        stk.push(i);
+    }
+    //вывод
+    for (int i=0; i < n; i++) {
+        cout << stk.top();
+        stk.pop();}
     return 0;
 }
