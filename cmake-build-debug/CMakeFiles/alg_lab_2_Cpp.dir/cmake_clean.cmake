22b298894acef3d310db065120eb647d5c0f8efd
optima211 <nedmk@mail.ru>
2018-12-23 22:13:52 +0700

    add start base files (part-21)

---
+++ cmake-build-debug/CMakeFiles/alg_lab_2_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alg_lab_2_Cpp.dir/main.cpp.obj"
+  "alg_lab_2_Cpp.pdb"
+  "alg_lab_2_Cpp.exe"
+  "alg_lab_2_Cpp.exe.manifest"
+  "alg_lab_2_Cpp.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/alg_lab_2_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
